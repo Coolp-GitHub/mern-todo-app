e1118dbae9947b2b2abc26d5e608ef62303b29be
Coolp-GitHub <75265429+Coolp-GitHub@users.noreply.github.com>
2023-10-04 14:00:55 -0400

    Working

---
+++ client/src/App.js
@@ -0,0 +1,119 @@
+import {useState, useEffect} from 'react';
+
+const API_BASE = "http://localhost:3001";
+
+function App() {
+	const[todos,setTodos] = useState([]);
+	const[popupActive, setPopupActive] = useState(false);
+	const[newTodo, setNewTodo] = useState("");
+
+	useEffect( () => {
+		GetTodos();
+	}, []);
+
+	const GetTodos =  () => {
+		console.log("GET");
+		fetch(API_BASE + "/todos")
+			.then(res => res.json())
+			.then(data => setTodos(data))
+			.catch(err =>console.error("Error: " , err));
+	}
+
+	const completeTodo = async id => {
+		console.log("COMPLETE");
+		const data = await fetch(API_BASE + "/todo/complete/" + id)
+			.then(res => res.json())
+		setTodos(todos => todos.map(todo =>{
+			if(todo._id === data._id){
+				todo.complete = data.complete; 
+			}
+			return todo;
+		}))
+	}
+
+	const deleteTodo = async (id,e) =>{
+		e.stopPropagation();
+		console.log("DEL");
+		const data = await fetch(API_BASE + "/todo/delete/" + id, {method: "DELETE"})
+			.then(res => res.json())
+
+		setTodos(todos => todos.filter(todos => todos._id !== data._id))
+	}
+	
+	const addTodo = async () =>{
+		console.log("ADD");
+		const data = await fetch(API_BASE + "/todo/new", {method:"POST",headers:{
+			"Content-Type":"application/json"
+		}, body: JSON.stringify({text: newTodo})})
+		.then(res => res.json())
+		setTodos([...todos, data]);
+		setPopupActive(false);
+		setNewTodo("");
+	}
+	return (
+		<div className="App">
+			<h1>Welcome</h1>
+			<h4>Your Tasks</h4>
+			<div className="todos">
+				{todos.length > 0 ? todos.map(todo => (
+					<div className=
+						{"todo" + (todo.complete ? " is-complete":"")} 
+						key={todo._id}
+						onClick={() => completeTodo(todo._id)}
+					>
+						<div className="checkbox"></div>
+						<div className="text">{ todo.text}</div>
+						<div className="delete-todo" onClick={(e) => deleteTodo(todo._id,e)}>x</div>
+					</div>
+				)) : (
+					<p className='noTasks'>You currently have no tasks</p>
+				)}
+			</div>
+
+			<div className="addPopup" onClick={() => setPopupActive(true)}>+</div>
+
+			{popupActive ? (
+				<div className='popup'>
+					<div className='closePopup'
+
+					 onClick={() => setPopupActive(false)}>x</div>
+
+					 <div className='content'>
+
+						<h3>Add Task</h3>
+						
+						<input type='text'
+						 className='add-todo-input'
+						 onChange={e => setNewTodo(e.target.value)}
+						 value={newTodo}/>
+
+						<div className='button' onClick={addTodo}> Create Task</div>
+					 </div>
+				</div>
+				):''
+			}
+		</div>
+	);
+}
+
+export default App;
+
+
+//651d947ebd1cc12469e398a2
+//651d944e39fe7dcd354df32f
+//651d944e39fe7dcd354df32f
+//651d9661522f4e2b6a08e1d1
+/*
+Failed to fetch
+TypeError: Failed to fetch
+    at addTodo (http://localhost:3000/static/js/bundle.js:54:24)
+    at HTMLUnknownElement.callCallback (http://localhost:3000/static/js/bundle.js:10994:18)
+    at Object.invokeGuardedCallbackDev (http://localhost:3000/static/js/bundle.js:11038:20)
+    at invokeGuardedCallback (http://localhost:3000/static/js/bundle.js:11095:35)
+    at invokeGuardedCallbackAndCatchFirstError (http://localhost:3000/static/js/bundle.js:11109:29)
+    at executeDispatch (http://localhost:3000/static/js/bundle.js:15253:7)
+    at processDispatchQueueItemsInOrder (http://localhost:3000/static/js/bundle.js:15279:11)
+    at processDispatchQueue (http://localhost:3000/static/js/bundle.js:15290:9)
+    at dispatchEventsForPlugins (http://localhost:3000/static/js/bundle.js:15299:7)
+    at http://localhost:3000/static/js/bundle.js:15459:16
+*/
